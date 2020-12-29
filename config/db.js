18d0bf3ecb7cda1825943915bf1228731d71c047
damarowen <damarowen@gmail.com>
2020-12-29 22:25:04 +0700

    new db

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);//* supaya ga error untuk findByidAndUpdate nya
 
+
+const db = process.env.DATABASE || 'mongodb://localhost:/newblogs';
+
 const connectDB = async () => {
-    const conn = await mongoose.connect('mongodb://localhost:/newblogs', {
+    const conn = await mongoose.connect(db, {
 		useNewUrlParser: true,
         useFindAndModify: false,
         useCreateIndex: true,
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 const app = express();
 const mongoose = require("mongoose");
 const connectDB = require('./config/db');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const session = require('express-session');
@@ -21,7 +21,7 @@ app.locals.moment = require('moment')
 // * Express Session
 
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!',
+    secret: process.env.SECRET || 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true
 }
